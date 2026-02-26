78fb0bb2cbfb6f4a633b255c49e5bd773946a324
Krishna-Jonnala <krishnajonnala33@gmail.com>
2026-02-26 16:13:45 +0530

    Krishna Added UC7-DequeOptimizedPalindrome

--- App/src/PalindroneCheckerApp.java
+++ App/src/PalindroneCheckerApp.java
@@ -1,41 +1,35 @@
 import java.util.Scanner;
-import java.util.Stack;
-import java.util.Queue;
+import java.util.Deque;
 import java.util.LinkedList;
 
-public class UseCase6PalindromeCheckerApp {
+public class UseCase7PalindromeCheckerApp {
 
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Welcome to Palindrome Checker App - UC6 (Queue + Stack)");
+        System.out.println("Welcome to Palindrome Checker App - UC7 (Deque Based)");
         System.out.print("Enter a string: ");
 
         String input = scanner.nextLine();
 
-        // Create Stack (LIFO)
-        Stack<Character> stack = new Stack<>();
+        // Create Deque
+        Deque<Character> deque = new LinkedList<>();
 
-        // Create Queue (FIFO)
-        Queue<Character> queue = new LinkedList<>();
-
-        // Push and Enqueue characters
+        // Insert characters into deque (rear)
         for (int i = 0; i < input.length(); i++) {
-            char ch = input.charAt(i);
-            stack.push(ch);      // LIFO
-            queue.add(ch);       // FIFO (Enqueue)
+            deque.addLast(input.charAt(i));
         }
 
         boolean isPalindrome = true;
 
-        // Compare Dequeue and Pop
-        for (int i = 0; i < input.length(); i++) {
+        // Compare front and rear elements
+        while (deque.size() > 1) {
 
-            char fromStack = stack.pop();      // Pop (LIFO)
-            char fromQueue = queue.remove();   // Dequeue (FIFO)
+            char front = deque.removeFirst();  // Remove from front
+            char rear = deque.removeLast();    // Remove from rear
 
-            if (fromStack != fromQueue) {
+            if (front != rear) {
                 isPalindrome = false;
                 break;
             }
